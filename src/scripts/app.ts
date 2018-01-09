ce31f5ba80b5120dcccfa9490149d0684e3bfcfe
SELISE\zahidur.rahman <zahidur.rahman@selise.ch>
2018-01-09 18:23:24 +0600

    Button Event

--- src/scripts/app.ts
+++ src/scripts/app.ts
@@ -1,3 +1,24 @@
+import '../styles/base.scss';
+
+import { Greeter } from './greeter';
+
+const greeter: Greeter = new Greeter('class1-assignment');
+const fullNameTextBox: HTMLElement = document.getElementById('fullName');
+const scoreTextBox: HTMLElement = document.getElementById('score');
+const addScoreButton: HTMLElement = document.getElementById('addScore');
+
+addScoreButton.addEventListener('click', ()=> {
+    console.log(fullNameTextBox.value);
+});
+
+
+const el = document.getElementById('greeting');
+if (el) {
+    el.innerHTML = greeter.greet();
+}
+
+
+
 /*
 import '../styles/base.scss';
 
@@ -51,13 +72,80 @@ world.color();
 
 
 //document.getElementById("demo").innerHTML  = greeter(<Student>user);
-import '../styles/base.scss';
 
-import { Greeter } from './greeter';
 
-const greeter: Greeter = new Greeter('class1-assignment');
-
-const el = document.getElementById('greeting');
-if (el) {
-    el.innerHTML = greeter.greet();
+/*interface Subject {
+    registerObserver(o: Observer): any;
+    removeObserver(o: Observer): any;
+    notifyObservers(): any;
 }
+
+interface Observer {
+    update(temperature: number): any;
+}
+
+// ------------------------------------------------------
+class WeatherStation implements Subject {
+    private observers: Observer[] = [];
+    private temperature: number;
+
+    registerObserver(o: Observer) {
+        this.observers.push(o);
+    }
+
+    removeObserver(o: Observer) {
+        let index = this.observers.indexOf(o);
+        this.observers.splice(index, 1);
+    }
+
+    notifyObservers() {
+        for (let observer of this.observers) {
+            observer.update(this.temperature);
+        }
+    }
+
+    setTemperature(temp: number) {
+        console.log('WeatherStation: new temperature measurement: ' + temp);
+        this.temperature = temp;
+        this.notifyObservers();
+    }
+}
+
+// ------------------------------------------------------
+class TemperatureDisplay implements Observer {
+    private subject: Subject;
+
+    constructor(weatherStation: Subject) {
+        this.subject = weatherStation;
+        weatherStation.registerObserver(this);
+    }
+
+    update(temperature: number) {
+        console.log('TemperatureDisplay: I need to update my display');
+    }
+}
+
+class Fan implements Observer {
+    private subject: Subject;
+
+    constructor(weatherStation: Subject) {
+        this.subject = weatherStation;
+        weatherStation.registerObserver(this);
+    }
+
+    update(temperature: number) {
+        if (temperature > 25) {
+            console.log('Fan: Its hot here, turning myself on...');
+        } else {
+            console.log('Fan: Its nice and cool, turning myself off...');
+        }
+    }
+}
+
+let weatherStation = new WeatherStation();
+
+let tempDisplay = new TemperatureDisplay(weatherStation);
+let fan = new Fan(weatherStation);
+
+weatherStation.setTemperature(20);
+weatherStation.setTemperature(30);*/
